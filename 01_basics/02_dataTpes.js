f2c3220acf735a1347f51d57d54e428cd2f9a7ee
Jagappa <118114433+Jagappa@users.noreply.github.com>
2025-07-17 06:31:16 +0000

    data

---
+++ 01_basics/02_dataTpes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all code as new ECMAScript version
+
+alert(3+3);     //we are  using node.js, not browser, so this will not show an alert box
+//shows alert box with message
+
+
+console.log(3+3);
+
+console.log("jaggu"); //logs to console
