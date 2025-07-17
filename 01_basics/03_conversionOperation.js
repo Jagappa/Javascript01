97dcd6d2250778ebda0e4bbd80a18ab823c4fd2b
Jagappa <118114433+Jagappa@users.noreply.github.com>
2025-07-17 06:44:35 +0000

    conversion of data

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score="33abc"
+console.log(typeof score);
+
+let valueInNumber=Number(score);//converts string to number
+// console.log(valueInNumber);33
+console.log(typeof valueInNumber); //number
+console.log(valueInNumber); // NAN
+
+// "33">> 33
+// "33abc">> NaN
+//true==> 1 false==> 0
+
